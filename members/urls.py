fdc478d4bfd64c32ce7dacce4d438caced9593d2
John B <broganjl@googlemail.com>
2026-02-12 17:00:54 +0000

    added password reset functionality

--- members/urls.py
+++ members/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path("login/", MemberLoginView.as_view(authentication_form=EmailOrAliasAuthenticationForm),name="login",),
     path("resend-verification/", resend_verification_email, name="resend-verification"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("password-reset/",auth_views.PasswordResetView.as_view(),name="password_reset",),
+    path("password-reset/done/",auth_views.PasswordResetDoneView.as_view(),name="password_reset_done",),
+    path("reset/<uidb64>/<token>/",auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm",),
+    path("reset/done/",auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete",),
 ]
